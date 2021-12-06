078ca72e7c397db418d69f34dc79430e4350bd2e
bcdat240601 <78672893+bcdat240601@users.noreply.github.com>
2021-12-06 14:48:40 +0700

    update code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,12 +7,18 @@ use App\Models\Product;
 use DB;
 class ProductController extends Controller
 {
-    public function ShowAll(){
-       $allproduct = DB::table('product')->get();
+    public function index(){
+       $allproduct = DB::table('product')->paginate(1);
+    //    return $this->pagination($allproduct);
        return response()->json([
             'status'=> 200,            
-            'allproduct'=> $allproduct,
-            'message'=> 'hello'
+            'allproduct'=> $allproduct,            
         ]);
     }
+    // public function pagination($truyvan,$page){
+    //     $quantity = $truyvan->count();
+    //     $number_page = ceil($quantity/$item_per_page);
+    //     $offset = ($page - 1)*$item_per_page;
+    //     $showitems = $truyvan->limit($item_per_page)->offset($offset)->get();
+    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('shop', [ProductController::class, 'ShowAll']);
+Route::post('shop', [ProductController::class, 'index']);
