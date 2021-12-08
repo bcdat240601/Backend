0e40e5edd98ebafd4a33d7177975ba4db4d65a5f
bcdat240601 <78672893+bcdat240601@users.noreply.github.com>
2021-12-08 11:29:10 +0700

    update code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,25 +23,38 @@ class ProductController extends Controller
         ]);
     }   
     public function showproductbycate(Request $req){
-        $array = [];
-        $product1 = DB::table('catesex')->select('catesex_id')->where('category_id',$req->id)->get();
-        $product2 = DB::table('category_detail');
-        foreach ($product1 as $values) {
+        // cach 1
+        // $product1 = DB::table('catesex')->select('catesex_id')->where('category_id',$req->id)->get();
+        // $product2 = DB::table('category_detail');
+        // foreach ($product1 as $values) {
+        //     foreach ($values as $value) {
+        //         $product2->orwhere('catesex_id' ,$value);
+        //     }
+        // }
+        // $result1 = $product2->select('cateagesex_id')->get();
+        // $product3 = DB::table('product');
+        // foreach ($result1 as $values) {
+        //     foreach ($values as $value) {
+        //         $product3->orwhere('cateagesex_id' ,$value);
+        //     }
+        // }
+        // $resul2 = $product3->paginate(1);
+        // return response()->json([
+        //     'status'=> 200,
+        //     'productcate'=> $resul2,
+        // ]);
+        //cach 2
+        $query1 = DB::table('catesex')->select('cateagesex_id')->join('category_detail','catesex.catesex_id','=','category_detail.catesex_id')->where('catesex.category_id',$req->id)->get();        
+        $query2 = DB::table('product');
+        foreach ($query1 as $values) {
             foreach ($values as $value) {
-                $product2->orwhere('catesex_id' ,$value);
+                $query2->orwhere('cateagesex_id' ,$value);
             }
         }
-        $result1 = $product2->select('cateagesex_id')->get();
-        $product3 = DB::table('product');
-        foreach ($result1 as $values) {
-            foreach ($values as $value) {
-                $product3->orwhere('cateagesex_id' ,$value);
-            }
-        }
-        $resul2 = $product3->paginate(1);
+        $result = $query2->paginate(1);
         return response()->json([
             'status'=> 200,
-            'productcate'=> $resul2,
+            'productcate'=> $result,
         ]);
     } 
     public function productdetail(Request $req){
